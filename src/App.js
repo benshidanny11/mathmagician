bc97bf5c405923e306d272c79822d691040e11e2
benshidanny11 <benshidanny11@gmail.com>
2022-05-24 11:02:53 +0200

    Implement feedback

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
 import Calculator from './components/Calculator';
 import './styles/main.css';
 
-class App {
+export default class App extends React.Component {
   render() {
-    (
+    return (
       <div className="App">
         <Calculator />
       </div>
     );
   }
 }
-
-export default App;
