9181fb0605eb8c373411f215ed688b169917f97f
benshidanny11 <benshidanny11@gmail.com>
2022-05-24 10:54:06 +0200

    Implement required changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Calculator from './components/Calculator';
 import './styles/main.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Calculator />
-    </div>
-  );
+class App {
+  render() {
+    (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
